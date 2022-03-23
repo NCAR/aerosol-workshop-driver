0ae4a5c52e1da18e90edcfb930f3dbfc3842d4d1
Matt Dawson <mattldawson@gmail.com>
2022-03-23 16:20:51 -0700

    rough in cpp driver

---
+++ interface/aerosol.cpp
@@ -0,0 +1,22 @@
+//===-- interface/aerosol.cpp -----------------------------------*- C++ -*-===//
+//
+// Copyright (C) 2022 National Center for Atmospheric Research
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file C++ wrapper for aerosol interface
+///
+//===----------------------------------------------------------------------===//
+
+#include "aerosol.hpp"
+
+namespace ai {
+
+Aerosol::Aerosol() {}
+
+void Aerosol::GetOptics(double *optics) {
+  // calculate optics
+}
+
+} // ai project
